39b559733ef64e508b4251c22edfb28f59a09606
Lakanavarapu Naga Manikanta Anudeep <61861542+anudeep586@users.noreply.github.com>
2022-07-15 19:28:38 +0530

    Update weather.test.ts

--- src/__tests__/weather.test.ts
+++ src/__tests__/weather.test.ts
@@ -1,10 +1,11 @@
 import { data } from '../data'
 import app  from '../server'
-import {getWDByCityService} from '../weatherService'
+import {addWeatherDetailsService, getWDByCityService} from '../weatherService'
 
 const request = require('supertest');
 
 describe('Weather data', () => {
+    let data:any;
     it("get  weather data by City Name", async () => {
         const res = await request(app.callback()).get(`/weather/New Delhi`)
 
@@ -16,8 +17,10 @@ describe('Weather data', () => {
         expect(res.status).toBe(200)
     })
     it("unit testing on getting data", async() => {
-        const res = await getWDByCityService('New Delhi')
+        const res1 = await addWeatherDetailsService('New Delhi')
 
-        expect(res).toBe(data)
+        const res = await getWDByCityService('New Delhi')
+        console.log(res,res1,"cool")
+        expect(res1).toEqual(expect.objectContaining({id:res.id}))
     })
 })
