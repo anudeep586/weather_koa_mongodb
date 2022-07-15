a0db5b4b28f02bfe0ece3c1758ffbf5b76cbe37d
Lakanavarapu Naga Manikanta Anudeep <61861542+anudeep586@users.noreply.github.com>
2022-07-15 19:27:35 +0530

    Update weatherController.ts

--- src/weatherController.ts
+++ src/weatherController.ts
@@ -1,6 +1,6 @@
 import {  v4 as uuidv4 } from "uuid";
 import weatherModel from "./models/weather";
-import { getWDByCityService } from "./weatherService";
+import { addWeatherDetailsService, getWDByCityService } from "./weatherService";
 
 
 const axios = require("axios");
@@ -9,13 +9,8 @@ const axios = require("axios");
 export const addWeatherDetails = async (ctx: any) => {
     try {
         const city=ctx.request.params.city;
-        const ApiKey = '412a8367b25c43da9cb71348220907'
-        let weatherdata = await axios.get(`http://api.weatherapi.com/v1/current.json`, {
-            params: { key: `${ApiKey}`, q: `${city}`, aqi: 'no' },
-        })
-        let data = weatherdata.data
-        const user=await weatherModel.create({ id: uuidv4(), cityName: data.location.name, country: data.location.country, temp_c: data.current.temp_c, temp_f: data.current.temp_f, humidity: data.current.humidity, wind_mph: data.current.wind_mph })
-        ctx.body = user
+        const data=addWeatherDetailsService(city)
+        ctx.body = data
     }
     catch (err) {
         ctx.body = "Not Found"
